867e627a33e7b865556f5a1b4f727894ce5716a7
pilarvermu <pilaribermu@gmail.com>
2024-08-31 01:27:21 +0200

    Añadidos evento e importado menu

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import "./style.css";
 
 import loadPage from "./header-footer";
 import loadHome from "./home";
+import loadMenu from "./menu";
+
 
 console.log("HOLA");
 
-loadPage();
-loadHome();
+init();
+
+function addEvents(){
+    const homeBtn = document.getElementById('home');
+    const menuBtn = document.getElementById('menu');
+
+    homeBtn.addEventListener('click', loadHome);
+    menuBtn.addEventListener('click', loadMenu);
+}
+
+function init(){
+    addEvents();
+    loadHome();
+}
+
